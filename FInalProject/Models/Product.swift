eced3448bfd716077cfe868116e7aab39ac59fed
Tumi Shoyoye <96660890+Oluwatumisesola@users.noreply.github.com>
2023-11-29 14:10:24 -0500

    issue1

---
+++ FInalProject/Models/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  FInalProject
+//
+//  Created by Oluwatumisesola on 11/29/23.
+//
+
+import Foundation
+
+struct Product: Identifiable {
+    var id = UUID()
+    var name: String
+    var image : String
+    var price: Int
+}
+
+var productlist = [Product (name: "Made By God | Emerald Green", image: "greenmbg", price: 100),
+                   Product (name: "Made By God | Desert Sand", image: "desertsandmbg", price: 100),
+                   Product (name: "Made By God | QR", image: "qr", price: 100)
+]
