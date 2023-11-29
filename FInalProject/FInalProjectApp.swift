cfb81fa03ffffad92da1838134e931ba7baf508c
Tumi Shoyoye <96660890+Oluwatumisesola@users.noreply.github.com>
2023-11-29 13:57:48 -0500

    Initial Commit

---
+++ FInalProject/FInalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FInalProjectApp.swift
+//  FInalProject
+//
+//  Created by Oluwatumisesola on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FInalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
