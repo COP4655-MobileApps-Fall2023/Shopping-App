cddb05bce8e56b00279da3878a1db8f404ff4215
Tumi Shoyoye <96660890+Oluwatumisesola@users.noreply.github.com>
2023-11-29 22:00:59 -0500

    cart added

---
+++ FInalProject/Components/CartButton.swift
@@ -0,0 +1,34 @@
+//
+//  CartButton.swift
+//  FInalProject
+//
+//  Created by Oluwatumisesola on 11/29/23.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing){
+            Image(systemName: "cart")
+                .padding(.top, 5)
+            
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2).bold()
+                    .foregroundColor(.white)
+                    .frame(width: 15, height: 15)
+                    .background(.red)
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+struct CartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButton(numberOfProducts:  1)
+    }
+}
--- FInalProject/Views/ContentView.swift
+++ FInalProject/Views/ContentView.swift
@@ -11,14 +11,21 @@ struct ContentView: View {
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(productList, id: \.id) { product in
-                    ProductCard(product: product)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                    }
                 }
+                .padding()
             }
-            .padding()
+            .navigationTitle("Dara Julo")
+            .toolbar {
+                CartButton(numberOfProducts: 1)
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
