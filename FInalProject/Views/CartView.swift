31aad2306795f98247746e0e986d62f91c62febf
Tumi Shoyoye <96660890+Oluwatumisesola@users.noreply.github.com>
2023-12-01 21:49:15 -0500

    Add to cart function is working

--- FInalProject/Views/CartView.swift
+++ FInalProject/Views/CartView.swift
@@ -11,11 +11,19 @@ struct CartView: View {
     @EnvironmentObject var cartManager: CartManager
     var body: some View {
         ScrollView{
-            if cartManager.products.count >0 {
+            if cartManager.products.count > 0 {
                 ForEach(cartManager.products, id: \.id){
                     product in
                     ProductRow(product: product)
                 }
+                
+                HStack {
+                    Text("Your cart total is")
+                    Spacer()
+                    Text("$\(cartManager.total).00")
+                        .bold()
+                }
+                .padding()
             } else {
                 Text("Your cart is empty")
             }
